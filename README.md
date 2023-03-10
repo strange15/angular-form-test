42c730ac1384f322968a5abf705077ca895c1185
strange <strange.wang@neutec.com.tw>
2023-03-10 18:05:38 +0800

    Template Driven Forms test

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # AngularFormTest
 
+Angular Form (Template Driven Forms / Reactive Forms) & TEST
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.8.
 
 ## Development server
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,88 @@
 import { Component } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-form-test';
+  // 綁定在帳號欄位上
+  account = '';
+
+  // 綁定在密碼欄位上
+  password = '';
+
+  // 帳號欄位的錯誤訊息
+  accountErrorMessage = '';
+
+  // 密碼欄位的錯誤訊息
+  passwordErrorMessage = '';
+
+  /**
+   * 綁定在帳號欄位上，當使用者改變登入帳號時，會觸發此函式，並取得對應的錯誤訊息
+   *
+   * @param {string} account
+   * @param {ValidationErrors} errors
+   */
+  accountValueChange(account: string, errors: ValidationErrors): void {
+    this.account = account;
+    this.validationCheck(errors, 'account');
+  }
+
+  /**
+   * 綁定在密碼欄位上，當使用者改變密碼時會觸發此函式
+   *
+   * @param {string} password
+   * @param {ValidationErrors} errors
+   */
+  passwordValueChange(password: string, errors: ValidationErrors): void {
+    this.password = password;
+    this.validationCheck(errors, 'password');
+  }
+
+  // 綁定在表單上，當使用者按下登入按鈕時會觸發此函式
+  login(): void {
+    // do login...
+  }
+
+  /**
+   * 透過欄位裡的 ValidationErrors 來設定該欄位的錯誤訊息
+   *
+   * @param {ValidationErrors} errors 欲驗證的欄位的錯誤 (by Angular)
+   * @param {'account' | 'password'} fieldName 欄位名稱
+   */
+  private validationCheck(
+    errors: ValidationErrors,
+    fieldName: 'account' | 'password'
+  ): void {
+    let errorMessage: string = '';
+    if (!errors) {
+      errorMessage = '';
+    } else if (errors['required']) {
+      errorMessage = '此欄位必填';
+    } else if (errors['pattern']) {
+      errorMessage = '格式有誤，請重新輸入';
+    } else if (errors['minlength']) {
+      errorMessage = '密碼長度最短不得低於8碼';
+    }
+    this.setErrorMessage(fieldName, errorMessage);
+  }
+
+  /**
+   * 設定指定欄位的錯誤訊息
+   *
+   * @param {'account' | 'password'} fieldName 欲設定錯誤訊息的欄位名稱
+   * @param {string} errorMessage 欲設定的錯誤訊息
+   */
+  private setErrorMessage(
+    fieldName: 'account' | 'password',
+    errorMessage: string
+  ): void {
+    if (fieldName === 'account') {
+      this.accountErrorMessage = errorMessage;
+    } else {
+      this.passwordErrorMessage = errorMessage;
+    }
+  }
 }
